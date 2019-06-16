99b9416030d1623dce9c8b89f152896a49d0d0df
LogicLost <logiclost00@gmail.com>
2019-06-16 08:00:46 -0400

    build.sh:update paths

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 make O=out ARCH=arm64 beryllium_defconfig
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CROSS_COMPILE_ARM32="/home/logic/Lost/arm32-gcc/bin/arm-eabi-"\
-                      CROSS_COMPILE="/home/logic/Lost/arm64-gcc/bin/aarch64-elf-"
+                      CROSS_COMPILE_ARM32="/home/logic/Lost/arm-eabi-gcc-9.x/bin/arm-eabi-"\
+                      CROSS_COMPILE="/home/logic/Lost/aarch64-elf-gcc-9.x/bin/aarch64-elf-"
